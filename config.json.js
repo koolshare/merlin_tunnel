31766b0877a0e266d71a90081abe73658dac16fb
janson <gandancing@gmail.com>
2016-11-27 20:35:24 +0800

    build ok

--- config.json.js
+++ config.json.js
@@ -1,6 +1,9 @@
 {
-"version":"0.5",
-"home_url":"Module_tunnel.asp",
-"title":"穿透DDNS",
-"description":"穿透 DDNS, 服务器转发方式"
+    "build_date": "2016-11-24_20:56:46", 
+    "description": "迅雷远程~", 
+    "home_url": "Module_thunder.asp", 
+    "md5": "24d7b2fd21e36251d33c4dc6b6d892fc", 
+    "module": "tunnel", 
+    "title": "迅雷远程", 
+    "version": "2.1"
 }
