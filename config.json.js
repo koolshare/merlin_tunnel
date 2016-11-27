095e5fd06038be415c18d42edd26116dbd6c1324
janson <gandancing@gmail.com>
2016-11-27 20:50:45 +0800

    change build.py to root directory

--- config.json.js
+++ config.json.js
@@ -1,7 +1,7 @@
 {
     "description": "穿透 DDNS, 服务器转发方式", 
     "home_url": "Module_tunnel.asp", 
-    "md5": "acee1a85f5557bde441274a932fcd0b1", 
+    "md5": "b4aee9896edc1ad59179980be2f6a31f", 
     "module": "tunnel", 
     "title": "穿透DDNS", 
     "version": "0.5"
