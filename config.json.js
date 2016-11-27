d9b23550ba778946d98709506dbf7ec6d60e09e1
janson <gandancing@gmail.com>
2016-11-27 20:52:28 +0800

    more information

--- config.json.js
+++ config.json.js
@@ -1,7 +1,7 @@
 {
     "description": "穿透 DDNS, 服务器转发方式", 
     "home_url": "Module_tunnel.asp", 
-    "md5": "b4aee9896edc1ad59179980be2f6a31f", 
+    "md5": "3dc5278a93990ef42c18366c828d760e", 
     "module": "tunnel", 
     "title": "穿透DDNS", 
     "version": "0.5"
